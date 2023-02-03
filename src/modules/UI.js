365b9ce560091180638bc24c572d67c46763d628
mubzie <rabiumubarak92@gmail.com>
2023-02-03 15:03:42 +0100

    fix: clean up the UI module and commented all the codes

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -2,44 +2,57 @@ import { PM } from "./PM";
 
 const displayProject = (project, i) => {
     
-    const sideBar = document.querySelector('.sidebar');
+    //target side navigation
+    const sideNav = document.querySelector('.sidebar');
     
+    //create project container div
     const projectContainer = document.createElement('div');
-    projectContainer.classList.add('project-container', 'add-project-btn', 'p-button');
+    projectContainer.classList.add('project-container', 'p-button');
     projectContainer.textContent = `${project.getProjectName()}`
 
+    //create a delete icon on each project container and set a data attribute to locate its id
     const delIcon = document.createElement('div');
     delIcon.textContent = 'x'
     delIcon.classList.add('del-btn')
     delIcon.setAttribute('data-id', `${i}`)
 
+    //append delete icon to parent container
     projectContainer.append(delIcon)
     
-    sideBar.appendChild(projectContainer)
+    //append project container to side navigation
+    sideNav.appendChild(projectContainer);
     
-    const buttons = document.querySelectorAll('.p-button');
+    //target all project container(buttons)
+    const Projectbtns = document.querySelectorAll('.p-button');
     
-    buttons.forEach( button => {
-        button.addEventListener('click', (e) => {
+    //iterates between each btns
+    Projectbtns.forEach( btn => {
+        btn.addEventListener('click', (e) => {
             e.stopImmediatePropagation()
             
+            //set project id with the currently selected project container
             document.getElementById('pid').value = project.id;
 
             console.log(project.id)
         })
     })
-
-
+    
+    
+    //delete each project with their respective id
     const delBtns = document.querySelectorAll('.del-btn');
 
+    //iterate between projects, locate the index and delete the project
     delBtns.forEach(btn => {
         btn.addEventListener('click', (e) => {
             e.stopImmediatePropagation()
 
-            const i = btn.dataset.id
+            //locating the index
+            const index = btn.dataset.id
             
-            PM.deleteProject(i)
+            //delete the project from projects array
+            PM.deleteProject(index)
 
+            //remove the project from the DOM
             btn.parentElement.remove();
 
         })
